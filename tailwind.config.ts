eee68dc391f37ea19dfddf505b54585ec2242989
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-01-02 23:13:37 -0300

    update config content, container, keyframes and animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default{
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     container: {
@@ -14,10 +15,7 @@ export default{
         sm: '2rem',
       },
       screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
+        DEFAULT: '1280px',
       }
     },
     fontFamily: {
@@ -27,15 +25,26 @@ export default{
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
 
     },
   },
   plugins: [
     require('@tailwindcss/container-queries'),
-    require('@tailwindcss/forms'),
     require('tailwind-scrollbar'),
   ],
 } satisfies Config
